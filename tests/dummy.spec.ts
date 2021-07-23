f1e0374c39145b35c2564f799059e12f45a63cd6
Raphael Castro <rafaphcastro@gmail.com>
2021-07-23 16:34:43 -0300

    chore: add sinon

--- tests/dummy.spec.ts
+++ tests/dummy.spec.ts
@@ -1,5 +1,19 @@
+import sinon from "sinon";
+
+class MyClass {
+    public myMethod1(): number {
+        return 123;
+    }
+
+    public myMethod2(): number {
+        return this.myMethod1();
+    }
+}
+
 describe("dummy tests", () => {
     it("is a dummy test", () => {
-        expect(true).to.be.true;
+        const instance = new MyClass();
+        sinon.stub(instance, "myMethod1").callsFake(() => 200);
+        expect(instance.myMethod2()).to.be.equal(200);
     });
 });
