fed65d09c4fc44e9c7ef367653a5bfedc4b64aa0
Raphael Castro <rafaphcastro@gmail.com>
2021-07-26 19:03:13 -0300

    feat: add cpf validator service

---
+++ src/services/cpf-validator.ts
@@ -0,0 +1,49 @@
+export class CpfValidator {
+  private sanitize(cpf: string): string {
+    return cpf.replace(/\D/g, "");
+  }
+
+  private isInvalidLenght(cpf: string): boolean {
+    return cpf.length !== 11;
+  }
+
+  private allNumberAreEqual(cpf: number[]): boolean {
+    return cpf.every((digit) => digit === cpf[0]);
+  }
+
+  private toNumbersArray(cpf: string): number[] {
+    return Array.from(cpf).map((digit) => parseInt(digit, 10));
+  }
+
+  public validate(cpf: string): boolean {
+    if (!cpf) {
+      return false;
+    }
+
+    cpf = this.sanitize(cpf);
+
+    if (this.isInvalidLenght(cpf)) {
+      return false;
+    }
+
+    const cpfArray = this.toNumbersArray(cpf);
+
+    if (this.allNumberAreEqual(cpfArray)) {
+      return false;
+    }
+
+    let checkerDigit1 = 0;
+    let checkerDigit2 = 0;
+
+    for (let i = 0; i < 9; i++) {
+      checkerDigit1 += cpfArray[i] * (10 - (i % 10));
+      checkerDigit2 += cpfArray[i] * (11 - (i % 11));
+    }
+
+    checkerDigit1 = (checkerDigit1 * 10) % 11;
+    checkerDigit2 += checkerDigit1 * 2;
+    checkerDigit2 = (checkerDigit2 * 10) % 11;
+
+    return checkerDigit1 === cpfArray[9] && checkerDigit2 === cpfArray[10];
+  }
+}
