3b197c16628ebe8a39d64ce3c1615d1acc3f3bba
Raphael Castro <rafaphcastro@gmail.com>
2021-07-26 19:04:10 -0300

    test: add cpf validator service

---
+++ tests/services/cpf-validator.spec.ts
@@ -0,0 +1,26 @@
+import { CpfValidator } from "@/services/cpf-validator";
+
+const validator = new CpfValidator();
+
+describe("CpfValidator", () => {
+  it("returns false when cpf is empty", () => {
+    expect(validator.validate("")).to.be.false;
+  });
+
+  it("returnn false when cpf is too big", () => {
+    expect(validator.validate("123456789012")).to.be.false;
+    expect(validator.validate("123.456.789-012")).to.be.false;
+  });
+
+  it("returns false when all numbers are equals", () => {
+    expect(validator.validate("11111111111")).to.be.false;
+  });
+
+  it("returns true for a valid cpf", () => {
+    expect(validator.validate("529.982.247-25")).to.be.true;
+  });
+
+  it("returns false for a invalid cpf", () => {
+    expect(validator.validate("529.982.147-25")).to.be.false;
+  });
+});
